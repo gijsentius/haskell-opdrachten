97c5e6510e891b5fae73ca51226432e63d22fffb
Gijs Entius <g.m.entius@gmail.com>
2021-03-02 21:32:10 +0100

    sync

--- Examples.hs
+++ Examples.hs
@@ -2,13 +2,44 @@ module Examples where
 
 import Data.Ratio
 
--- Operator priority test
-between x = 2 < x && x < 8
-
 --  import test
 breuk x y = x % y
+
+-- Werkt niet
+positief x
+  | x < 0 = - x
+  | x >= 0 = x
+
+-- Voorbeeld van een hogere orde functie (een functie in een functie)
+diff f =    f'
+    where   f' x  = (f (x-h) - f x) / h
+            h   = 0.0001
+
+-- Voorbeeld van een recursieve implementatie van machtsverheffen
+power x 0 = 1
+power x y = x * power x (y-1)
+
+power' x n
+    | n == 0 = 1
+    | otherwise = x * power' x (n-1)
+
+-- Operator priority test
+between x = 2 < x && x < 8
 
 -- Lengte van een lijst
 lengte lijst
   | null lijst = 0
   | otherwise = 1 + lengte (tail lijst)
+
+-- test met foldr
+-- VS code geeft hier een hint die niet werkt
+eenVanDe = (||) False
+
+eenVanDe' :: Foldable t => t Bool -> Bool
+eenVanDe' xs = foldr (||) False xs
+
+-- Test met map
+verdubbel x = map (*2) x
+
+-- samenstellen van functies met . voorbeeld
+noteven n = (not . even) n
--- Opdrachten.hs
+++ Opdrachten.hs
@@ -1,17 +1,89 @@
 module Opdrachten where
 
--- opdracht 1.5
+-- determinant a b c = square b - 4 * a * c
+
+-- opgave 1.2
+aantalOpl a b c
+  | d > 0 = 3
+  | d == 0 = 2
+  | d < 0 = 1
+  where
+    d = b ^ 2 - 4 * a * c
+
+-- opgave 1.3
+fac 0 = 1
+fac x = x * fac (x -1)
+
+fac' x
+  | x == 0 = 1
+  | otherwise = x * fac (x -1)
+
+-- opgave 1.4
+-- een voorbeeld is logischerwijs de tweede afgeleide en .. 
+
+-- opgave 1.5
 lijst = 1 : [1] -- Voor het toevoegen van een element
 
 lijst2 :: [Integer] -- type definition
 lijst2 = [1, 2] ++ [1] -- Voor het samenvoegen van lijsten
 
--- opdracht 1.6 a
+-- opgave 1.6 a
 som xs
   | xs == [] = 0
   --   of null xs
   | otherwise = head xs + som (tail xs)
 
--- opdracht 1.6 b
+-- opgave 1.6 b
 som' [] = 0
 som' (x : xs) = x + som xs
+
+-- opgave 1.7 a
+eenVanDe xs
+  | null xs = False
+  | otherwise = head xs || eenVanDe (tail xs)
+
+-- opgave 1.7 b
+eenVanDe' [] = False
+eenVanDe' (x:xs) = x || eenVanDe' xs
+
+-- opgave 1.7 c
+eenVanDe'' xs = foldr (||) False xs
+
+-- opgave 1.8
+-- map :: (a -> b) -> [a] -> [b]
+
+-- opgave 1.9 a
+-- quot :: Integral a => a -> a -> a
+-- div :: Integral a => a -> a -> a
+-- quot en div hebben een type variabele a uit klasse Integral
+
+-- opgave 1.9 b
+-- De typen die gebruikt kunnen worden door quot en div zijn dus Int en Integer,
+-- want die vallen onder de klasse Integral
+
+-- opgave 1.9 c
+-- 7.3 :: Fractional p => p
+
+-- opgave 1.9 d
+-- Double en Float
+
+-- opgave 1.10
+-- Door www.haskell.org/haskellwiki/Monomorphism_restriction
+-- werkt dit niet bij mijn versie van ghci
+-- Foldable t => (a -> b -> b) -> b -> t a -> b
+som'' :: Num b => [b] -> b
+som'' = foldr (+) 0
+
+-- opgave 1.11
+oneven :: Integral a => a -> Bool
+oneven = not . even
+-- om te testen of het inderdaad hetzelfde is
+oneven' :: Integral x => x -> Bool
+oneven' x = (not . even) x
+
+-- opgave 1.12
+-- map (^2) [1,2,3,..,10]
+
+-- opgave 1.13
+-- f (x, y) betekent dat f een tupel verwacht. 
+-- f is in haskell een functie die een parameter heeft namelijk de tupel.
